66c7b97311f515b063a1d4fa9b1a8f6a2d54475b
bearddan2000 <bearddan2000@yahoo.com>
2023-03-19 00:02:02 -0400

    initial commit

---
+++ py-srv/bin/data.py
@@ -0,0 +1,26 @@
+from settings import COLUMN_NAME
+
+INDEX_NAME = "pop-demo"
+
+DOC = [
+    {
+        COLUMN_NAME['col_0']['name']: 1, 
+        COLUMN_NAME['col_1']['name']: 'Orange', 
+        COLUMN_NAME['col_2']['name']: 'orange'
+    },
+    {
+        COLUMN_NAME['col_0']['name']: 2, 
+        COLUMN_NAME['col_1']['name']: 'Grape', 
+        COLUMN_NAME['col_2']['name']: 'purple'
+    },
+    {
+        COLUMN_NAME['col_0']['name']: 3, 
+        COLUMN_NAME['col_1']['name']: 'Blueberry', 
+        COLUMN_NAME['col_2']['name']: 'blue'
+    },
+    {
+        COLUMN_NAME['col_0']['name']: 4, 
+        COLUMN_NAME['col_1']['name']: 'Strawberry', 
+        COLUMN_NAME['col_2']['name']: 'red'
+    }
+]
